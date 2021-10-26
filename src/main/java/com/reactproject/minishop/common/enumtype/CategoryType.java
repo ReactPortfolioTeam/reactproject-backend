d7e7260bc5acb71b222b00a57c2ba2a15318bc0c
lookhkh <lookhkh37@gmail.com>
2021-10-26 21:44:22 +0900

    카테고리 타입 정의 수정

--- src/main/java/com/reactproject/minishop/common/enumtype/CategoryType.java
+++ src/main/java/com/reactproject/minishop/common/enumtype/CategoryType.java
@@ -1,6 +1,36 @@
 package com.reactproject.minishop.common.enumtype;
 
+
+/*
+ * 0 : 하의 28,30,32,34....
+ * 1 : 상의 s,m,l,xl,xxl
+ * 2 : sizefree : 단순 stock
+ * 
+ * */
 public enum CategoryType {
-	BOTTOMS, SHIRTS, TEES, FLEECE, KNITS, OUTERWEAR, FOOTWEAR,
-	ACCESSORIES, LIFESTYLE, HOME
+	BOTTOMS(0,"bottoms"), SHIRTS(1,"shirts"), TEES(1,"tees"),
+	FLEECE(1,"fleece"), KNITS(1,"knits"), OUTERWEAR(1,"outerwear"), 
+	FOOTWEAR(2,"footwear"),ACCESSORIES(2,"accessories"), 
+	LIFESTYLE(2,"lifestyle"), HOME(2,"home");
+	
+	private CategoryType(int code, String name) {
+		
+	}
+	
+	private String name;
+	private int code;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
 }
